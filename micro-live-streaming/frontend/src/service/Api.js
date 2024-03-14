c06295e2c2cb99e4d9d30b55a5fe7b2ceb38197b
Vinay Singh <vinayksingh2@gmail.com>
2024-03-14 19:34:05 +0530

    remove stirng

--- micro-live-streaming/frontend/src/service/Api.js
+++ micro-live-streaming/frontend/src/service/Api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export const base = Axios.create({
-  baseURL: `https://stream-api.fantv.world/api`,
+  baseURL: `${process.env.REACT_APP_MICRO_BACKEND_MANAGER_URL}/api`,
   headers: {
     "Content-Type": "application/json"
   }
